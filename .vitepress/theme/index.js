712140e2d5a4512dd1e8d3d1034cb967d3db806e
Strawberry <165325276+sahar-km@users.noreply.github.com>
2025-07-05 12:27:00 +0330

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,8 +1,10 @@
 import DefaultTheme from 'vitepress/theme';
+import './custom.css';
 import 'viewerjs/dist/viewer.min.css';
 import imageViewer from 'vitepress-plugin-image-viewer';
 import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
 import { useRoute } from 'vitepress';
+
 
 export default {
     ...DefaultTheme,
