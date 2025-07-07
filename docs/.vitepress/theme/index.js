698a45964534ac3c541b2af526a2fb514ea4373d
Strawberry <165325276+sahar-km@users.noreply.github.com>
2025-07-08 03:18:18 +0330

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,19 +1,19 @@
-import DefaultTheme from 'vitepress/theme';
-import './custom.css';
-import 'viewerjs/dist/viewer.min.css';
-import imageViewer from 'vitepress-plugin-image-viewer';
-import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
-import { useRoute } from 'vitepress';
+import DefaultTheme from 'vitepress/theme'
+import './styles.css'
+import 'viewerjs/dist/viewer.min.css'
+import imageViewer from 'vitepress-plugin-image-viewer'
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue'
+import { useRoute } from 'vitepress'
 
 
 export default {
     ...DefaultTheme,
     enhanceApp(ctx) {
-        DefaultTheme.enhanceApp(ctx);
-        ctx.app.component('vImageViewer', vImageViewer);
+        DefaultTheme.enhanceApp(ctx)
+        ctx.app.component('vImageViewer', vImageViewer)
     },
     setup() {
-        const route = useRoute();
-        imageViewer(route);
+        const route = useRoute()
+        imageViewer(route)
     }
-};
+}
